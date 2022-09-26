7a2f59916c1f59a6a0c08260e8ac1b741f6aa87e
harcourtian <suryanshhbtu@gmail.com>
2022-09-26 10:59:52 +0530

    Semi-completed Project

---
+++ app/src/main/java/com/example/greenridershbtu/Cycle.java
@@ -0,0 +1,25 @@
+package com.example.greenridershbtu;
+
+
+    public class Cycle {
+        String _id;
+        String cycleid;
+        String status;
+        String stdid;
+
+        public String get_id() {
+            return _id;
+        }
+
+        public String getCycleid() {
+            return cycleid;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public String getStdid() {
+            return stdid;
+        }
+    }
---
+++ app/src/main/java/com/example/greenridershbtu/RetrofitInterface.java
@@ -0,0 +1,72 @@
+package com.example.greenridershbtu;
+
+import java.util.HashMap;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface RetrofitInterface {
+
+    @POST("/users/login")
+    Call<LoginResult> executeLogin(@Body HashMap<String, String> map);
+    // LoginResult -> return
+    //  email aur password hashmap me store kiya hai
+
+//    @POST("/user/signup")
+//    Call<Void> executeSignup (@Body HashMap<String, String> map);
+
+    //    @GET(("/cycles/"+str)
+    @GET("/cycles/{cycleid}")
+    Call<Cycle> getCycle(@Header("Authorization") String AUTH_KEY, @Path("cycleid") String cycleid);
+
+
+    //    @FormUrlEncoded
+    @PATCH("/users/{id}")
+    Call<Void> setRented(@Path("id") String _id,
+                         @Body HashMap<String, String> map);
+
+    //    @FormUrlEncoded
+    @PATCH("/users/{stdid}")
+    Call<Void> removeRented(@Path("stdid") String abc,
+                            @Body HashMap<String, String> map);
+
+    @PATCH("/cycles/{cycleid}")
+    Call<Void> setRentedUser(@Header("Authorization") String AUTH_KEY,@Path("cycleid") String cycleid,
+                             @Body HashMap<String, String> map);
+
+    @PATCH("/cycles/{cycleid}")
+    Call<Void> removeRentedUser(@Header("Authorization") String AUTH_KEY,@Path("cycleid") String cycleid,
+                                @Body HashMap<String, String> map);
+
+
+    @POST("/users/signup")
+    Call<Void> executeSignup(@Body HashMap<String, String> map);
+
+    @POST("/cycles/add")
+    Call<Void> executeCycleSignup(@Body HashMap<String, String> map);
+
+    @DELETE("/cycles/{cycleid}")
+    Call<Void> executeDeleteCycle(@Path("cycleid") String abc);
+
+
+    @DELETE("/users/{email}")
+    Call<Void> executeDelete(@Path("email") String abc);
+
+
+    @PATCH("/users/password/{id}")
+    Call<Void> executeChangePassword(@Path("id") String abc,@Body HashMap<String, String> map);
+
+
+
+    @GET("/cycles/all")
+    Call<List<Cycle>> getRentedCycle(@Header("Authorization") String AUTH_KEY);
+//    @PATCH("cycles/{cycleid}")
+//    Call<Cycle> getCycle(@Path("cycleid") String id);
+}
